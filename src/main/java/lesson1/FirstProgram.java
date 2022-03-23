9e3f7623dc3b0089deaf35a9e869c33bb7b2c24d
snaiperRus <snaiperRus87@gmail.com>
2022-03-23 23:51:43 +0300

    add first program

--- src/main/java/lesson1/FirstProgram.java
+++ src/main/java/lesson1/FirstProgram.java
@@ -2,6 +2,6 @@ package lesson1;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world!123");
+        System.out.println("Hello, world!");
     }
 }
